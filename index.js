2e99ef52bc7bc4bfd429e401b81a488136878f9c
Andrea <greenlounge@hotmail.com>
2019-08-06 16:57:22 -0500

    Beer and code: Jugando con APIs y bases de datos

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+let todo = [
+    {
+        title: "Lavar ropa",
+        description: "Hay que laver sol la ropa negra",
+        date: "2019-07-10",
+        done: false
+    }
+]
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        data: todo.map((e, i) => {
+            return {
+                id: i,
+                ...e
+            }
+        })
+    })
+})
+
+app.get ('/:id', (req, res) => {
+    res.json({
+        data: todo[req.params.id]
+    })
+})
+
+app.post('/', (req, res) => {
+    todo.push(req.body)
+    res.send('SAVED!')
+})
+
+app.delete('/:id', (req, res) => {
+    todo = todo.filter((e,i)=>{
+    return Number(i) !== Number (req.params.id)
+    })
+    res.send('DELETED')
+})
+
+app.patch('/', (req, res) => {
+    const {id, done} = req.body
+    todo = todo.map((e, i) => {
+        if (Number(id) === Number(i)) {
+            e.done = done
+        }
+        return e
+    })
+    res.send('PATCH')
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
